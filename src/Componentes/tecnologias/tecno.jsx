1158268e0f09341bdffc3c236bb9692368dea7e5
unknown <yan-franco>
2023-02-07 18:14:17 -0500

    tecnologias completas

--- src/Componentes/tecnologias/tecno.jsx
+++ src/Componentes/tecnologias/tecno.jsx
@@ -1,7 +1,225 @@
 import React from "react";
+import "./tecno.css"
 
 export default function tecno() {
     return(
-        <h1>Tecnologías</h1>
+        <div className="conta">
+            <h1>Tecnologías con las que me siento cómodo:</h1>
+            <div className="card">
+                <div className="face face1">
+                    <div className="content">
+                        <i class="fab fa-js"></i>
+                        <h3>JavaScript</h3>
+                    </div>
+                </div>
+                <div className="face face2">
+                    <div className="content">
+                        <p>JavaScript es un lenguaje de programación interpretado, dialecto del estándar ECMAScript. Se define como orientado a objetos, basado en prototipos, imperativo, débilmente tipado y dinámico.</p> 
+
+                        <a href="https://www.javascript.com/" type="button">Leer más</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="card">
+                <div className="face face1">
+                    <div className="content">
+                        <i className="fab fa-html5"></i>
+                        <h3>Html</h3>
+                    </div>
+                </div>
+                <div className="face face2">
+                    <div className="content">
+                        <p>HTML, siglas en inglés de HyperText Markup Language, hace referencia al lenguaje de marcado para la elaboración de páginas web.</p> 
+
+                        <a href="https://html.com/" type="button">Leer más</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="card">
+                <div className="face face1">
+                    <div className="content">
+                        <i className="fab fa-css3-alt"></i>
+                        <h3>Css</h3>
+                    </div>
+                </div>
+                <div className="face face2">
+                    <div className="content">
+                        <p>CSS, en español «Hojas de estilo en cascada», es un lenguaje de diseño gráfico para definir y crear la presentación de un documento estructurado escrito en un lenguaje de marcado.</p> 
+
+                        <a href="https://lenguajecss.com/css/introduccion/que-es-css/" type="button">Leer más</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="card">
+                <div className="face face1">
+                    <div className="content">
+                        <i class="fab fa-react"></i>
+                        <h3>React</h3>
+                    </div>
+                </div>
+                <div className="face face2">
+                    <div className="content">
+                        <p>React es una biblioteca Javascript de código abierto diseñada para crear interfaces de usuario con el objetivo de facilitar el desarrollo de aplicaciones en una sola página. Es mantenido por Facebook y la comunidad de software libre. En el proyecto hay más de mil desarrolladores libres.</p> 
+
+                        <a href="https://es.reactjs.org/" type="button">Leer más</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="card">
+                <div className="face face1">
+                    <div className="content">
+                        <h3>Redux</h3>
+                    </div>
+                </div>
+                <div className="face face2">
+                    <div className="content">
+                        <p>Redux es una librería JavaScript de código abierto para el manejo del estado de las aplicaciones. Es comúnmente usada con otras librerías como React o Angular para la construcción de Interfaces de Usuario.</p> 
+
+                        <a href="https://redux.js.org/" type="button">Leer más</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="card">
+                <div className="face face1">
+                    <div className="content">
+                        <i class="fab fa-node-js"></i>
+                        <h3>Node</h3>
+                    </div>
+                </div>
+                <div className="face face2">
+                    <div className="content">
+                        <p>Node.js es un entorno de servidor de código abierto multiplataforma que puede ejecutarse en Windows, Linux, etc. Node.js es un entorno de tiempo de ejecución de JavaScript de back-end, se ejecuta en el motor de JavaScript V8 y ejecuta el código de JavaScript fuera de un navegador web.</p> 
+
+                        <a href="https://nodejs.org/es/" type="button">Leer más</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="card">
+                <div className="face face1">
+                    <div className="content">
+                        <h3>Express</h3>
+                    </div>
+                </div>
+                <div className="face face2">
+                    <div className="content">
+                        <p>Express.js o simplemente Express, es un entorno de trabajo para aplicaciones web para el programario Node.js, de código abierto y con licencia MIT. Se utiliza para desarrollar aplicaciones web y APIs. El autor original es TJ Holowaychuk y la primera versión se lanzó el 2010.</p> 
+
+                        <a href="https://expressjs.com/es/" type="button">Leer más</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="card">
+                <div className="face face1">
+                    <div className="content">
+                        <h3>PostgreSQL</h3>
+                    </div>
+                </div>
+                <div className="face face2">
+                    <div className="content">
+                        <p>PostgreSQL, también llamado Postgres, es un sistema de gestión de bases de datos relacional orientado a objetos y de código abierto, publicado bajo la licencia PostgreSQL, similar a la BSD o la MIT.</p> 
+
+                        <a href="https://www.postgresql.org/" type="button">Leer más</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="card">
+                <div className="face face1">
+                    <div className="content">
+                        <i class="fab fa-bootstrap"></i>
+                        <h3>Bootstrap</h3>
+                    </div>
+                </div>
+                <div className="face face2">
+                    <div className="content">
+                        <p>Bootstrap es una biblioteca multiplataforma o conjunto de herramientas de código abierto para diseño de sitios y aplicaciones web.</p> 
+
+                        <a href="https://getbootstrap.com/" type="button">Leer más</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <h1>Tecnologías que tengo pensado aprender:</h1>
+
+
+            <div className="card">
+                <div className="face face1">
+                    <div className="content">
+                        <i class="fab fa-java"></i>
+                        <h3>Java</h3>
+                    </div>
+                </div>
+                <div className="face face2">
+                    <div className="content">
+                        <p>Java es un lenguaje de programación y una plataforma informática que fue comercializada por primera vez en 1995 por Sun Microsystems.</p> 
+
+                        <a href="https://www.java.com/es/" type="button">Leer más</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="face face1">
+                    <div className="content">
+                        <h3>TypeScript</h3>
+                    </div>
+                </div>
+                <div className="face face2">
+                    <div className="content">
+                        <p>TypeScript es un lenguaje de programación libre y de código abierto desarrollado y mantenido por Microsoft. Es un superconjunto de JavaScript, que esencialmente añade tipos estáticos y objetos basados en clases.</p>
+
+                        <a href="https://www.typescriptlang.org/" type="button">Leer más</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="face face1">
+                    <div className="content">
+                        <i class="fab fa-angular"></i>
+                        <h3>Angular</h3>
+                    </div>
+                </div>
+                <div className="face face2">
+                    <div className="content">
+                        <p>Angular es un framework para aplicaciones web desarrollado en TypeScript, de código abierto, mantenido por Google, que se utiliza para crear y mantener aplicaciones web de una sola página.</p> 
+
+                        <a href="https://angular.io/" type="button">Leer más</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="face face1">
+                    <div className="content">
+                        <i class="fab fa-python"></i>
+                        <h3>Python</h3>
+                    </div>
+                </div>
+                <div className="face face2">
+                    <div className="content">
+                        <p>Python es un lenguaje de programación de propósito general y alto nivel. Su filosofía de diseño enfatiza la legibilidad del código con el uso de sangría significativa. Python se escribe dinámicamente y se recolecta basura.</p> 
+
+                        <a href="https://www.python.org/" type="button">Leer más</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
